376f4a46946fa7523c99fba7cbd3a0a0c3325e65
techsavvysos <kedogosospeter36@gmail.com>
2023-04-22 00:54:57 +0100

    added icons

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
-import marked from 'marked';
+import {marked} from 'marked';
+import { FaExpandArrowsAlt } from 'react-icons/fa';
+import { FaFireAlt } from 'react-icons/fa';
 import './Editor.css'
 
 export default function Editor() {
 
-    const [markdown, setMarkDown] = useState();
+    const [markdown, setMarkDown] = useState("");
 
 
 
@@ -12,18 +14,22 @@ export default function Editor() {
         <div>
 
             <div className='editorWrap'>
-            <div class="toolbar">
-                <button>resize</button>
+            <div class="toolbar">  
+                <p className='label'><FaFireAlt /> Editor</p>
+                <FaExpandArrowsAlt className='icon' />
             </div>
-            <textarea id="editor" className='editor' onChange={e => setMarkDown(e.target.value)} ></textarea>
+            <textarea id="editor" className='editor' onChange={e => setMarkDown(e.target.value)} >
+                
+            </textarea>
             </div>
 
             <div className='previewWrap'>
                 <div className='toolbar'>
-                <button>resize</button>
+                    <p className='label'><FaFireAlt/> Previewer</p>
+                <FaExpandArrowsAlt className='icon' />
                 </div>
                 <div className='preview' dangerouslySetInnerHTML={{__html: marked(markdown)}}>
-        
+                    
                 </div>
             </div>
 
